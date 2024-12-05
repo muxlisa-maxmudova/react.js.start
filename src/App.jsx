82106dcbcbafe8c3ac39fa31108c316c44f20e09
muxlisa-maxmudova <akobirurozov22@gmail.com>
2024-12-05 21:51:51 +0500

    footer end

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Header from "./components/Header.jsx";
-import MainContent from "./components/MainContent.jsx";
+import Footer from "./components/Footer.jsx";
+//import MainContent from "./components/MainContent.jsx";
 function App() {
     return (
         <div>
-            <MainContent/>
+            <Footer/>
         </div>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <p> ©️ My web-site 2024</p>
+        </footer>
+    )
+}
+export default Footer
