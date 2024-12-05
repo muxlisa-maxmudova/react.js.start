f6f8fb8f18f45f753ac81ed4ed70fd5e53fca356
muxlisa-maxmudova <akobirurozov22@gmail.com>
2024-12-05 21:55:32 +0500

    components are integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Footer from "./components/Footer.jsx";
-//import MainContent from "./components/MainContent.jsx";
+import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
 function App() {
     return (
         <div>
+            <Header/>
+            <MainContent/>
             <Footer/>
         </div>
     )
