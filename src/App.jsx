8a39e1a8a3f0ae77f73d626fa312f0d4bc1ab6f5
muxlisa-maxmudova <akobirurozov22@gmail.com>
2024-12-05 21:47:15 +0500

    mainContent end

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
 function App() {
     return (
         <div>
-            <Header/>
+            <MainContent/>
         </div>
     )
 }
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+//import React from 'react'
+
+const MainContent = () => {
+    return (
+        <main style={{ background: "lightsalmon", flexDirection: "column" }}>
+            <h1>Information about Lorem Ipsum</h1>
+            <p>Lorem ipsum (/ˌlɔː.rəm ˈɪp.səm/ LOR-əm IP-səm) is a dummy or placeholder text commonly used in graphic design, publishing, and web development to fill empty spaces in a layout that do not yet have content. <br/>
+
+                Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, <br/> with words altered, added, and removed to make it nonsensical <br/> and improper Latin. The first two words themselves are a truncation of dolorem ipsum.
+
+                Versions of the Lorem ipsum text have been used in typesetting at least since the 1960s, when it was popularized by advertisements for Letraset transfer <br/> sheets.[1] Lorem ipsum was introduced to the digital world <br/> in the mid-1980s, when Aldus employed it in graphic and word-processing templates for its desktop publishing program PageMaker. Other popular word processors, including Pages and Microsoft Word, have since adopted Lorem ipsum,[2] as have many LaTeX packages,[3][4][5] web content managers such as Joomla! and WordPress, and CSS libraries such as Semantic UI.</p>
+        </main>
+    )
+}
+export default MainContent
