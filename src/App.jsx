cfe65a87c0d763a619c02a3b95e02b3268b17695
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-01-26 13:53:44 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-//import QuizApp from "./SmallProjects/QuizApp.jsx";
-import ProductList from "./Lists/ProductList.jsx";
-import QuizApp from "./SmallProjects/QuizApp.jsx";
 export const App = () => {
     return (
         <main>
-         <QuizApp/>
+
         </main>
     )
 }
