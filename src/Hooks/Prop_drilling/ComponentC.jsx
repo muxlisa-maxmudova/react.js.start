a572361695d7a7f251202b99f2f5fcd9c8d4f43c
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-01-26 15:13:16 +0500

    UseContext_end

--- src/Hooks/Prop_drilling/ComponentC.jsx
+++ src/Hooks/Prop_drilling/ComponentC.jsx
@@ -1,24 +1,27 @@
 import {Data} from "../../App.jsx";
 import {Data1} from "../../App.jsx";
+import {useContext} from "react";
 const ComponentC = () => {
+    const userName = useContext(Data);
+    const userAge = useContext(Data1);
     return (
-
-            <Data.Consumer>
-                {(name)=>{
-                    return (
-                        <Data1.Consumer>
-                            {
-                                (age)=>{
-                                return(
-                                    <h1>Age:{age} && Name:{name} </h1>
-                                      )
-                                }
-                            }
-                        </Data1.Consumer>
-                        )
-                    }
-                }
-            </Data.Consumer>
+        <h1>My name is {userName} and i am {userAge}</h1> // easier and compact way to transmit data
+            // <Data.Consumer>
+            //     {(name)=>{
+            //         return (
+            //             <Data1.Consumer>
+            //                 {
+            //                     (age)=>{
+            //                     return(
+            //                         <h1>Age:{age} && Name:{name} </h1>
+            //                           )
+            //                     }
+            //                 }
+            //             </Data1.Consumer>
+            //             )
+            //         }
+            //     }
+            // </Data.Consumer>
 
     )
 }
