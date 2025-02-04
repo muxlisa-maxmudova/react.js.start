bb95513825ae58e4c8d816266f5719da70bcc9d4
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-02-04 21:52:29 +0500

    minorChanges

--- src/Hooks/UseRef/UseRef.jsx
+++ src/Hooks/UseRef/UseRef.jsx
@@ -4,20 +4,36 @@ const UseRef = () => {
     //const background = useRef(null)
 
         const background = useRef(false)
+    const color = useRef(false)
         const handleClick = () => {
             if(background.current) {
                 document.body.style.backgroundColor = 'black'
-                document.body.style.backgroundColor = "white"; // Change text color to white for contrast
+                document.body.style.backgroundColor = "white";
+
+                // Change text color to white for contrast
             }else {
                 document.body.style.backgroundColor = 'white';
                 document.body.style.backgroundColor = "black";
             }
            background.current = !background.current;
+
+        }
+        const handleText =() => {
+            if(color.current){
+                document.h1.style.color = 'black'
+                document.h1.style.color = 'white'
+            }else {
+                document.h1.style.color = 'white'
+                document.h1.style.color = 'black'
+            }
+            color.current = !color.current;
         }
 
     return (
         <div>
+            <h1>Hello</h1>
             <button ref={background} onClick={handleClick}>Dark</button>
+            <button ref={color} onClick={handleText}>dark</button>
        </div>
 )
 }
